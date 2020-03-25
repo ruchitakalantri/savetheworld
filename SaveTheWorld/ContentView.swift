c065981bc689965bc40e7a28290fc2e34cfc8c27
Ruchita Kalantri <rkalantri.kalantri@yahoo.com>
2020-03-25 12:58:25 -0700

    Initial Commit

---
+++ SaveTheWorld/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SaveTheWorld
+//
+//  Created by Ruchita Rathi on 3/25/20.
+//  Copyright © 2020 Updatus. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
